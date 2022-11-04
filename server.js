f1fc4c3efaa9ed0ce84442d92faa1368f04d75d3
Amitoj Singh <asingh1160@myseneca.ca>
2022-11-04 16:02:50 -0400

    111

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/about', (req, res) => {
 
     res.render('about', {
         //data: someData,
-        layout: true // do not use the default Layout (main.hbs)
+        //layout: true // do not use the default Layout (main.hbs)
     });
 });
 
